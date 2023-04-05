b0740d19bba803b744759f55a59d2c3cb288fef2
Annie <owangeshi@gmail.com>
2023-04-05 04:18:53 -0700

    2 Task

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _puts_recursion - function like puts();
- * @s: pointer to string.
+ * @s: string
  *
  * Return: Always 0(Success)
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') /* base case */
+	if (*s == 0) 
 	{
 		 _putchar('\n');
 		 return;
