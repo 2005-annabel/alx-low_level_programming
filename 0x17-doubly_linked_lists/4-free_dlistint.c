ba91bb73f92a1e0982857f06ebde5578ce9a423a
2005-annabel <owangeshi@gmail.com>
2023-08-17 14:00:13 -0700

    4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees list
+ * @head: head of list to free
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+	tmp = head;
+	while (tmp != NULL)
+	{
+		free(tmp);
+		tmp = tmp->next;
+	}
+}
